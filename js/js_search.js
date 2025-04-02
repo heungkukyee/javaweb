a2a3f040e849fe8f5ffd4fa3a2b048534926cfde
heungkukyee <silkroad_jk@naver.com>
2025-04-03 03:13:34 +0900

    4주차 연습 문제 적용

--- js/js_search.js
+++ js/js_search.js
@@ -1,8 +1,20 @@
-document.getElementById("search_btn").addEventListener('click', search_message);
+document.getElementById("search_button_msg").addEventListener('click', search_message);
 
 function search_message(){
     alert("검색을 수행합니다!");
     }
+
+function search_message(){
+    let message = "검색을 수행합니다.";
+    alert(message);
+    }
+
+/*
+function search_message(){
+
+    }
+같은 이름의 함수 여러 개 만들면 가장 마지막 함수 수행
+*/
 
 function googleSearch(){
     const searchTerm = document.getElementById("search_input").value; //검색어로 설정
