1dd8890afd75a8a46e1684cb8e9b4f62b233c941
heungkukyee <silkroad_jk@naver.com>
2025-04-10 01:22:23 +0900

    문법 오류 수정

--- js/js_data_type.js
+++ js/js_data_type.js
@@ -26,21 +26,24 @@ const obj2 = {
 console.log(symbolVar1.toString()); //문자열 변환 출력
 console.log(obj1, obj2, airline); //여러 개 출력
 
-const users = new Map(); //사용자 정보 Map객체 생성
+//Map객체 생성
+const users = new Map();
 users.set("user1", {
     id: 1, password: "password123",
 });
+//user1은 key, {}안은 value
+
 users.set("user2", {
     id: 2, password: "password456"
 });
 
-//Map 객체의 모든 사용자 정보 반복 출력
+//[key, value] 순서로 꺼내기
 for (const[username, user] of users) {
-    console.log('사용자 이름: ${username}', 'ID: ${user.id}');
-    console.log('비밀번호: ${userpassword}');
+    console.log(`사용자 이름: ${username}`, `ID: ${user.id}`);
+    console.log(`비밀번호: ${user.password}`);
 }
 
-//Set 객체 활용(예), 이름만 저장할 Set객체 생성
+//Set 객체 생성, 이름 저장
 const usernames = new Set();
 
 usernames.add("user1"); // 사용자 이름 추가
